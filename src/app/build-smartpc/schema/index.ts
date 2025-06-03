ca703314b7cb7254e9cc8326aeded0b42b401d97
wali  ahmad <waliahmadfiles@gmail.com>
2025-06-03 19:19:06 +0500

    update

--- src/app/build-smartpc/schema/index.ts
+++ src/app/build-smartpc/schema/index.ts
@@ -1,7 +1,14 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-  pcName: z.string().min(1, "PC name is required"),
+  pcName: z
+    .string()
+    .min(1, "PC name is required")
+    .max(50, "PC name must be at most 50 characters")
+    .regex(
+      /^(?![_-])(?!.*[_-]{2})[A-Za-z0-9_-]+(?<![_-])$/,
+      "PC name can only contain letters, numbers, dash (-), underscore (_), and no spaces or consecutive special characters"
+    ),
   operatingSystem: z.string(),
   cpu: z.string(),
   // memory: z.string(),
