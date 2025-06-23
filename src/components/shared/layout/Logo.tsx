305f78a75b8c422b6ac8fabcb9c581464bd27446
Atif Arshad <atifarshad0315@gmail.com>
2025-06-24 01:05:12 +0500

    add a custom css class

--- src/components/shared/layout/Logo.tsx
+++ src/components/shared/layout/Logo.tsx
@@ -4,12 +4,9 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center space-x-2">
       <span
-        className="font-bold text-2xl tracking-tight text-transparent transition-all duration-300 hover:from-[#0284C7] hover:to-[#4F46E5]"
+        className="text-gradient-clip font-bold text-2xl tracking-tight text-transparent transition-all duration-300 hover:from-[#0284C7] hover:to-[#4F46E5]"
         style={{
           backgroundImage: "linear-gradient(to right, #0284C7,#4F46E5)",
-          WebkitBackgroundClip: "text",
-          backgroundClip: "text",
-          WebkitTextFillColor: "transparent",
         }}
       >
         SmartPC
