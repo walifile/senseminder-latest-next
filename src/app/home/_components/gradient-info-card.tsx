1e89e1a50f5bd49cd2e1dbb212fed7916c92d28e
Wali Ahmad <waliahmadfiles@gmail.com>
2025-12-05 04:40:16 +0500

    pys

--- src/app/home/_components/gradient-info-card.tsx
+++ src/app/home/_components/gradient-info-card.tsx
@@ -26,8 +26,8 @@ export function GradientInfoCard({
       {/* <div className="pointer-events-none absolute inset-0 rounded-3xl bg-[radial-gradient(circle_at_top,_#5B2FFF_0%,_#0B1020_45%,_transparent_70%)] opacity-70" /> */}
 
       {/* CARD */}
-      <div className="relative flex h-full glass-card !rounded-3xl border border-white/5 p-6 md:p-8 text-left gap-5">
-        <div className="absolute top-0 right-0 w-52 h-52 opacity-70 bg-gradient-to-br from-blue-700 to-sky-300 rounded-full blur-[100px]" />
+      <div className="relative overflow-hidden flex h-full rounded-[20px] border border-[rgba(37,48,240,0.20)] dark:border-[rgba(255,255,255,0.20)] bg-white dark:bg-[rgba(255,255,255,0.03)] backdrop-blur-[32px] p-6 md:p-8 text-left gap-5">
+        <div className="absolute top-6 right-[-120px] w-[300px] h-[120px] opacity-60 bg-[rgba(13,0,255,0.60)] rounded-[300px] blur-[71px]" />
         <div
           className={`relative flex-shrink-0 w-full ${
             rightImage ? "md:w-[55%]" : ""
@@ -43,7 +43,7 @@ export function GradientInfoCard({
             {children}
           </div>
         </div>
-        <div className="absolute bottom-0 left-0 w-52 h-52 opacity-70 bg-gradient-to-br from-blue-700 to-sky-300 rounded-full blur-[100px]" />
+        <div className="absolute bottom-[-90px] left-[-140px] w-[301px] h-[255px] bg-[rgba(77,0,255,0.60)] opacity-60 rounded-[301px] blur-[71px] -z-10" />
 
         {/* Conditional Rendering of the Right Image */}
         {rightImage && (
