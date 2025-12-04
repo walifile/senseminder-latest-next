9b22b435494c539d521ca963d16a477515628020
Wali Ahmad <waliahmadfiles@gmail.com>
2025-12-04 18:46:50 +0500

    up

--- src/app/dashboard/sense-cloud/_components/storage-sidebar.tsx
+++ src/app/dashboard/sense-cloud/_components/storage-sidebar.tsx
@@ -41,15 +41,15 @@ export function SidebarPanel({
   return (
     <div className="w-full md:w-[265px] p-6 pr-4 md:border-r border-border">
       <header className="inline-flex flex-col items-start gap-1 mb-12">
-        <h1 className="w-fit font-space-grotesk font-bold text-2xl md:text-3xl mt-[-1.00px] text-white whitespace-nowrap">
+        <h1 className="w-fit font-space-grotesk font-bold text-2xl md:text-3xl mt-[-1.00px] text-[#020816] dark:text-white whitespace-nowrap">
           Sense Cloud
         </h1>
-        <p className="w-fit text-base text-[#b8c2d5] whitespace-nowrap">
+        <p className="w-fit text-base text-[#454545] dark:text-[#b8c2d5] whitespace-nowrap">
           Manage your files and folders
         </p>
       </header>
       <div className="mb-8">
-        <Card className="bg-[#ffffff08] border-[#ffffff1a] !rounded-[10px] !md:rounded-xl w-full">
+        <Card className="bg-[rgba(37,48,240,0.07)] border-[rgba(37,48,240,0.10)] dark:bg-[#ffffff08] dark:border-[#ffffff1a] !rounded-[10px] !md:rounded-xl w-full">
           <CardContent className="flex flex-col gap-4 p-4">
             <div className="flex items-center justify-between gap-6">
               <span className="text-base font-semibold">Storage</span>
@@ -68,7 +68,11 @@ export function SidebarPanel({
               <>
                 <Progress
                   value={usagePercentage}
-                  className={`h-2 ${usagePercentage > 90 ? "bg-red-200" : "bg-[#FFFFFF1A]"}`}
+                  className={`h-2 ${
+                    usagePercentage > 90
+                      ? "bg-red-200"
+                      : "bg-[#ced1fc] dark:bg-[#FFFFFF1A]"
+                  }`}
                 />
                 {usagePercentage > 90 && (
                   <div className="flex items-center gap-2 mt-2 text-xs text-red-500">
@@ -81,7 +85,7 @@ export function SidebarPanel({
           </CardContent>
         </Card>
 
-        <div className="grid grid-cols-2 gap-2 mt-6">
+        <div className="grid grid-cols-1 gap-2 mt-6">
           <Button
             size="default"
             variant="default"
@@ -90,15 +94,6 @@ export function SidebarPanel({
           >
             <Upload className="h-4 w-4" />
             Upload
-          </Button>
-          <Button
-            size="default"
-            variant="outline"
-            className="w-full"
-            onClick={onNewFolderClick}
-          >
-            <FolderPlus className="h-4 w-4" />
-            New
           </Button>
         </div>
       </div>
@@ -116,8 +111,8 @@ export function SidebarPanel({
                 }
                 className={`w-full justify-start ${
                   selectedCategory === category.name
-                    ? "text-white"
-                    : "text-[#B9C2D5]"
+                    ? "text-[#020816] dark:text-white border border-[#2530F0] bg-[rgba(37,48,240,0.10)] dark:border-[rgba(255,255,255,0.10)] dark:bg-[#ffffff08]"
+                    : "text-[#454545] dark:text-[#B9C2D5] hover:text-[#020816] hover:border hover:border-[#2530F0] hover:bg-[rgba(37,48,240,0.10)] dark:hover:text-white dark:hover:border-transparent dark:hover:bg-[#ffffff08]"
                 }`}
                 onClick={() => onSelectCategory(category.name)}
               >
