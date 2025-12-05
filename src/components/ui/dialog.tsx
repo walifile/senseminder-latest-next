242fd7d8ffab30f2b6896a9b31d323e454706aee
Wali Ahmad <waliahmadfiles@gmail.com>
2025-12-05 15:31:15 +0500

    df

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -45,7 +45,7 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {/* Gradient Layer */}
-      <div className="w-[247px] h-[403px] left-[-96px] top-[-202px] absolute dark:hidden bg-blue-700/20 rounded-[50%] blur-3xl" />
+      <div className="w-[247px] h-[403px] left-[-96px] top-[-202px] absolute dark:hidden bg-blue-700/20 rounded-[50%] blur-3xl overflow-hidden" />
 
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
@@ -106,10 +106,7 @@ const DialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Description
     ref={ref}
-    className={cn(
-      "text-sm text-[#454545] dark:text-[#A3A3A3]",
-      className
-    )}
+    className={cn("text-sm text-[#454545] dark:text-[#A3A3A3]", className)}
     {...props}
   />
 ));
