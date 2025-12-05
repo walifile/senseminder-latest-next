e1e673dfe7daebca884f3358643316a484f2b476
Wali Ahmad <waliahmadfiles@gmail.com>
2025-12-05 05:07:44 +0500

    sdf

--- src/app/about/_components/mission-card.tsx
+++ src/app/about/_components/mission-card.tsx
@@ -14,7 +14,7 @@ export function MissionCard() {
   return (
     <section>
       <div className="container z-10 relative mb-16 md:mb-32">
-        <div className="grid gap-8 md:grid-cols-2 items-stretch">
+        <div className="grid gap-10 md:grid-cols-2 items-stretch">
           {/* Our Mission */}
           <GradientInfoCard
             title="Our Mission"
@@ -88,22 +88,21 @@ export function MissionCard() {
               title="Our Story"
               rightImage="/assets/svg/about/our-story.svg"
             >
-              <p className="self-stretch justify-start text-paragraph text-lg font-normal font-['Inter'] leading-8">
+              <p className="self-stretch justify-start text-paragraph text-2xl font-normal font-['Inter'] leading-8">
                 In 2020, I (Ashfak Ahmed) saw a frustrating truth: even the most
                 talented users were still limited by their hardware.
               </p>
 
-              <p className="self-stretch justify-start text-paragraph text-lg font-normal font-['Inter'] leading-8">
+              <p className="self-stretch justify-start text-paragraph text-2xl font-normal font-['Inter'] leading-8">
                 So I built Senseminder — a platform designed from the cloud up
                 to offer people what they truly need: flexibility, power, and
                 freedom.
               </p>
 
-              <div className="relative md:w-fit mt-12">
-                <div className="z-0 absolute left-1/2 top-2.5 -translate-x-1/2 w-[50%] h-[40px] blur-[35px] bg-[linear-gradient(270deg,_#A801BA_0%,_#2530F0_100%)]" />
+              <div className="relative md:w-fit pt-12">
                 <Button
                   size="default"
-                  className="relative w-full z-10"
+                  className="relative w-full !py-4 h-auto"
                   onClick={onGetStarted}
                 >
                   Learn More
