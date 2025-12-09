2dbe6e13f2a6ea34e6ed7facb4e8cde4bf927433
Wali Ahmad <waliahmadfiles@gmail.com>
2025-12-09 14:24:12 +0500

    up

--- src/app/home/_components/gradient-info-card.tsx
+++ src/app/home/_components/gradient-info-card.tsx
@@ -59,7 +59,7 @@ export function GradientInfoCard({
         </div>
         {!rightImage && (
           // Light theme bottom glow
-          <div className="absolute bottom-[0px] left-[-140px] w-[301px] h-[255px] bg-[rgba(77,0,255,0.60)] opacity-60 rounded-[301px] blur-[71px]  -z-10 dark:hidden" />
+          <div className="absolute top-[270px] left-[-127px] w-[301px] h-[255px] bg-[rgba(77,0,255,0.60)] opacity-[0.3] rounded-[301px] blur-[142px] -z-10 dark:hidden" />
         )}
         {!rightImage && (
           // Dark theme bottom circular gradient glow
