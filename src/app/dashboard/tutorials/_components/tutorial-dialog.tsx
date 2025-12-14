3306266c7b63315f54f95356fd5b076e1b9105db
Wali Ahmad <89452621+walifile@users.noreply.github.com>
2025-12-15 03:50:18 +0500

    df

--- src/app/dashboard/tutorials/_components/tutorial-dialog.tsx
+++ src/app/dashboard/tutorials/_components/tutorial-dialog.tsx
@@ -48,7 +48,7 @@ export const TutorialDialog = ({ tutorial }: TutorialDialogProps) => (
               priority
             />
           </div>
-          <div className="absolute top-3 right-3 bg-black/80 text-white px-2.5 py-1 rounded-full text-xs font-semibold">
+          <div className="absolute top-3 right-3 bg-[#00000030] text-white px-4 py-1 rounded-[999999px] text-base font-semibold backdrop-blur-sm">
             {tutorial.duration}
           </div>
         </div>
